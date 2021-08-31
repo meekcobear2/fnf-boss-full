775518146cc989401e03632c7e23b81fa4b344dd
meekcobear2 <repuzzleyt@gmail.com>
2021-08-30 20:55:20 -0600

    publishing shit

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -39,6 +39,7 @@ class HealthIcon extends FlxSprite
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
 		animation.add('boss', [24, 25], 0, false, isPlayer);
 		animation.add('chaosboss', [26, 27], 0, false, isPlayer);
+		animation.add('insaneboss', [24, 25], 0, false, isPlayer);
 		animation.play(char);
 
 		switch(char)
